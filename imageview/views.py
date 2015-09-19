5210f6b38b3010a46e7bd2df1be1032743453cb8
w00tw00tw00tw00t <w00tw00t@xandev.nl>
2015-09-19 18:08:41 +0200

    Rudimentary filtering - it doesn't scale though.

--- imageview/views.py
+++ imageview/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.edit import FormView
 from django.views.generic import ListView
+from taggit.models import Tag
 from .forms import ImageForm
 from .models import Image
 
@@ -18,3 +19,9 @@ class UploadView(FormView):
 class MainView(ListView):
     template_name = 'imageview/list.html'
     model = Image
+
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context.update({"tags": Tag.objects.all()})
+
+        return context
